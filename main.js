1e0efb8a21fcf8b8e37c584b79f1948fbf746eca
BenBrucker <BenBruecker@gmail.com>
2015-01-22 09:02:06 +0100

    test commit

--- main.js
+++ main.js
@@ -3,5 +3,6 @@
  * Nothing special to see here, it updates the map,
  * calls the screep controller and then spawns new creeps.
  */
+/* Test comment */
 var controller = require('game_controller');
 controller();
